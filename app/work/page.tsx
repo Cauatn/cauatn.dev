805faa5cea781b01c429f135df6f1da45fa13ca9
Cauã Tavares <tavarescauac@gmail.com>
2024-05-31 21:17:02 -0300

    feat: Update work page with details

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -16,39 +16,6 @@ export default function Work() {
           está um resumo do que fiz até agora.
         </p>
         <hr className="my-6 border-neutral-200 dark:border-neutral-800" />
-        <h2 className="font-medium text-xl mb-1 tracking-tighter">Scan.ai</h2>
-        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-          frontend engineer, 2023 — 2024
-        </p>
-        <p>
-          Entrei na <a href="/">Scan.ai</a> como FullStack e atuei no
-          desenvolvimento de interfaces de usuário, incluindo páginas de marca,
-          formulários, painéis de controle da empresa.
-        </p>
-        <ul>
-          <li>
-            Successfully converted wireframe designs into active working user
-            interface components.
-          </li>
-          <li>Developed rule-based form builder for brands.</li>
-          <li>
-            Developed and implemented user and enterprise dashboard interface,
-            including all related pages, for user interaction and data
-            visualization.
-          </li>
-          <li>
-            Stayed abreast of emerging trends and best practices in front-end
-            development, continually honing skills and exploring innovative
-            solutions to technical challenges.
-          </li>
-          <li>
-            Developed the{" "}
-            <a href="https://app.utilify.xyz/ucl">Campaign page</a> for the
-            souvenir NFT distributed for the UCL final in partnership with
-            Turkish Airlines.
-          </li>
-        </ul>
-        <hr className="my-6 border-neutral-200 dark:border-neutral-800" />
         <h2 className="font-medium text-xl mb-1 tracking-tighter">Scasys</h2>
         <p className="text-neutral-600 dark:text-neutral-400 text-sm">
           programador e pesquisador, 2023 — 2024
@@ -75,6 +42,35 @@ export default function Work() {
             desenvolvimento, aprimorando continuamente habilidades e explorando
             inovações soluções para desafios técnicos.
           </li>
+        </ul>
+        <hr className="my-6 border-neutral-200 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Scan.ai</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          frontend engineer, 2023 — 2024
+        </p>
+        <p>
+          Entrei na <a href="/">Scan.ai</a> como um dos desenvolvedores que
+          atuaram na criação desse produto.
+        </p>
+        <ul>
+          <li>Construindo.</li>
+          {/*<li>Developed rule-based form builder for brands.</li>
+          <li>
+            Developed and implemented user and enterprise dashboard interface,
+            including all related pages, for user interaction and data
+            visualization.
+          </li>
+          <li>
+            Stayed abreast of emerging trends and best practices in front-end
+            development, continually honing skills and exploring innovative
+            solutions to technical challenges.
+          </li>
+          <li>
+            Developed the{" "}
+            <a href="https://app.utilify.xyz/ucl">Campaign page</a> for the
+            souvenir NFT distributed for the UCL final in partnership with
+            Turkish Airlines.
+          </li> */}
         </ul>
       </div>
     </Container>
